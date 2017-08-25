288a2026cba61303cd8222214e5fd071494f9163
Neil Zimmerman <Neil@mbp-13-mid-2012.attlocal.net>
2017-08-24 23:53:16 -0500

    resolved several issues

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 var fighters = [
   {
     name: "Terra",
-    hp: 153,
+    hp: 199,
     attack: 14,
     counterAttack: 12,
     alive: true,
@@ -15,7 +15,7 @@ var fighters = [
     name: "Yuffie",
     hp: 219,
     attack: 22,
-    counterAttack: 19,
+    counterAttack: 18,
     alive: true,
     victories: 0,
     losses: 0
@@ -38,11 +38,6 @@ var fighters = [
     losses: 0
   }
 ];
-
-var currentCharacterName = "";
-var currentEnemyName = "";
-var currentEnemyHP = 0;
-var currentEnemyCounterAttack = 0;
 
 var currentEnemyIndex = 99;
 var currentCharacterIndex = 99;
@@ -88,51 +83,55 @@ $("#tidusImage").hover (
 //Character Selection Click Functions
 
 $("#terraImage").click (function() {
-	characterSelected = true;
-	currentCharacterIndex = 0;
-	currentCharacterName = fighters[0].name;
-	$("#chooseYourWarrior").html(currentCharacterName);
-	$("#enemy1").attr("src", "assets/images/characters/yuffie.png");
-	$("#enemy2").attr("src", "assets/images/characters/cloud.png");
-	$("#enemy3").attr("src", "assets/images/characters/tidus.png");
-	$("#selectedCharacter").attr("src", "assets/images/characters/terra.png");
-	$("#currentEnemy").attr("src", "assets/images/chooseopponent.jpg");
+	if (!characterSelected) {
+		characterSelected = true;
+		currentCharacterIndex = 0;
+		$("#chooseYourWarrior").html(fighters[currentCharacterIndex].name);
+		$("#enemy1").attr("src", "assets/images/characters/yuffie.png");
+		$("#enemy2").attr("src", "assets/images/characters/cloud.png");
+		$("#enemy3").attr("src", "assets/images/characters/tidus.png");
+		$("#selectedCharacter").attr("src", "assets/images/characters/terra.png");
+		$("#currentEnemy").attr("src", "assets/images/chooseopponent.jpg");
+	}
 })
 
 $("#yuffieImage").click (function() {
-	characterSelected = true;
-	currentCharacterIndex = 1;
-	currentCharacterName = fighters[1].name;
-	$("#chooseYourWarrior").html(currentCharacterName);
-	$("#enemy1").attr("src", "assets/images/characters/terra.png");
-	$("#enemy2").attr("src", "assets/images/characters/cloud.png");
-	$("#enemy3").attr("src", "assets/images/characters/tidus.png");
-	$("#selectedCharacter").attr("src", "assets/images/characters/yuffie.png");
-	$("#currentEnemy").attr("src", "assets/images/chooseopponent.jpg");
+	if (!characterSelected) {
+		characterSelected = true;
+		currentCharacterIndex = 1;
+		$("#chooseYourWarrior").html(fighters[currentCharacterIndex].name);
+		$("#enemy1").attr("src", "assets/images/characters/terra.png");
+		$("#enemy2").attr("src", "assets/images/characters/cloud.png");
+		$("#enemy3").attr("src", "assets/images/characters/tidus.png");
+		$("#selectedCharacter").attr("src", "assets/images/characters/yuffie.png");
+		$("#currentEnemy").attr("src", "assets/images/chooseopponent.jpg");
+	}
 })
 
 $("#cloudImage").click (function() {
-	characterSelected = true;
-	currentCharacterIndex = 2;
-	currentCharacterName = fighters[2].name;
-	$("#chooseYourWarrior").html(currentCharacterName);
-	$("#enemy1").attr("src", "assets/images/characters/terra.png");
-	$("#enemy2").attr("src", "assets/images/characters/yuffie.png");
-	$("#enemy3").attr("src", "assets/images/characters/tidus.png");
-	$("#selectedCharacter").attr("src", "assets/images/characters/cloud.png");
-	$("#currentEnemy").attr("src", "assets/images/chooseopponent.jpg");
+	if (!characterSelected) {
+		characterSelected = true;
+		currentCharacterIndex = 2;
+		$("#chooseYourWarrior").html(fighters[currentCharacterIndex].name);
+		$("#enemy1").attr("src", "assets/images/characters/terra.png");
+		$("#enemy2").attr("src", "assets/images/characters/yuffie.png");
+		$("#enemy3").attr("src", "assets/images/characters/tidus.png");
+		$("#selectedCharacter").attr("src", "assets/images/characters/cloud.png");
+		$("#currentEnemy").attr("src", "assets/images/chooseopponent.jpg");
+	}
 })
 
 $("#tidusImage").click (function() {
-	characterSelected = true;
-	currentCharacterIndex = 3;
-	currentCharacterName = fighters[parseInt(currentCharacterIndex)].name;
-	$("#chooseYourWarrior").html(fighters[currentCharacterIndex].name);
-	$("#enemy1").attr("src", "assets/images/characters/terra.png");
-	$("#enemy2").attr("src", "assets/images/characters/yuffie.png");
-	$("#enemy3").attr("src", "assets/images/characters/cloud.png");
-	$("#selectedCharacter").attr("src", "assets/images/characters/tidus.png");
-	$("#currentEnemy").attr("src", "assets/images/chooseopponent.jpg");
+	if (!characterSelected) {
+		characterSelected = true;
+		currentCharacterIndex = 3;
+		$("#chooseYourWarrior").html(fighters[currentCharacterIndex].name);
+		$("#enemy1").attr("src", "assets/images/characters/terra.png");
+		$("#enemy2").attr("src", "assets/images/characters/yuffie.png");
+		$("#enemy3").attr("src", "assets/images/characters/cloud.png");
+		$("#selectedCharacter").attr("src", "assets/images/characters/tidus.png");
+		$("#currentEnemy").attr("src", "assets/images/chooseopponent.jpg");
+	}
 })
 
 
@@ -140,24 +139,24 @@ $("#tidusImage").click (function() {
 
 
 //Choosing Opponents//
-if (!enemySelected) {
+
 $("#enemy1").click (function() {
 	
-	enemySelected = true;
-	$("#enemy1").attr("src", "assets/images/placeholder.jpg");
-	
-	if (characterSelected && currentCharacterName === fighters[0].name) {	
-		currentEnemyName = fighters[1].name;
-		currentEnemyIndex = 1;
-		$("#currentEnemy").attr("src", "assets/images/characters/yuffie.png");
-		$("#hpLevels").html("<br>" + fighters[currentEnemyIndex].name + "'s HP: " + fighters[1].hp);
-	}
+	if (!enemySelected) { 
+		enemySelected = true;
+		$("#enemy1").attr("src", "assets/images/placeholder.jpg");
+		
+		if (characterSelected && currentCharacterIndex === 0) {	
+			currentEnemyIndex = 1;
+			$("#currentEnemy").attr("src", "assets/images/characters/yuffie.png");
+			$("#hpLevels").html("<br>" + fighters[currentEnemyIndex].name + "'s HP: " + fighters[1].hp);
+		}
 
-	else if (characterSelected) {
-		currentEnemyName = fighters[0].name;
-		currentEnemyIndex = 0;
-		$("#currentEnemy").attr("src", "assets/images/characters/terra.png");
-		$("#hpLevels").html("<br>" + fighters[currentEnemyIndex].name + "'s HP: " + fighters[0].hp);
+		else if (characterSelected) {
+			currentEnemyIndex = 0;
+			$("#currentEnemy").attr("src", "assets/images/characters/terra.png");
+			$("#hpLevels").html("<br>" + fighters[currentEnemyIndex].name + "'s HP: " + fighters[0].hp);
+		}
 	}
 })
 
@@ -166,28 +165,27 @@ $("#enemy1").click (function() {
 
 $("#enemy2").click (function() {
 	
-	enemySelected = true;
-	$("#enemy2").attr("src", "assets/images/placeholder.jpg");
-	
-	if (characterSelected && currentCharacterName === fighters[0].name) {	
-		currentEnemyName = fighters[2].name;
-		currentEnemyIndex = 2;
-		$("#currentEnemy").attr("src", "assets/images/characters/cloud.png");
-		$("#hpLevels").html("<br>" + fighters[2].name + "'s HP: " + fighters[2].hp);
-	}
+	if (!enemySelected) {
+		enemySelected = true;
+		$("#enemy2").attr("src", "assets/images/placeholder.jpg");
+		
+		if (characterSelected && currentCharacterIndex === 0) {	
+			currentEnemyIndex = 2;
+			$("#currentEnemy").attr("src", "assets/images/characters/cloud.png");
+			$("#hpLevels").html("<br>" + fighters[2].name + "'s HP: " + fighters[2].hp);
+		}
 
-	else if (characterSelected && currentCharacterName === fighters[1].name) {
-		currentEnemyName = fighters[2].name;
-		currentEnemyIndex = 2;
-		$("#currentEnemy").attr("src", "assets/images/characters/cloud.png");
-		$("#hpLevels").html("<br>" + fighters[2].name + "'s HP: " + fighters[2].hp);
-	}
+		else if (characterSelected && currentCharacterIndex === 1) {
+			currentEnemyIndex = 2;
+			$("#currentEnemy").attr("src", "assets/images/characters/cloud.png");
+			$("#hpLevels").html("<br>" + fighters[2].name + "'s HP: " + fighters[2].hp);
+		}
 
-	else if (characterSelected) {
-		currentEnemyName = fighters[1].name;
-		currentEnemyIndex = 1;
-		$("#currentEnemy").attr("src", "assets/images/characters/yuffie.png");
-		$("#hpLevels").html("<br>" + fighters[1].name + "'s HP: " + fighters[1].hp);
+		else if (characterSelected) {
+			currentEnemyIndex = 1;
+			$("#currentEnemy").attr("src", "assets/images/characters/yuffie.png");
+			$("#hpLevels").html("<br>" + fighters[1].name + "'s HP: " + fighters[1].hp);
+		}
 	}
 })
 
@@ -196,25 +194,23 @@ $("#enemy2").click (function() {
 
 $("#enemy3").click (function() {
 	
-	enemySelected = true;
-	$("#enemy3").attr("src", "assets/images/placeholder.jpg");
-	
-	if (characterSelected && currentCharacterName === fighters[3].name) {	
-		currentEnemyName = fighters[2].name;
-		currentEnemyIndex = 2;
-		$("#currentEnemy").attr("src", "assets/images/characters/tidus.png");
-		$("#hpLevels").html("<br>" + fighters[2].name + "'s HP: " + fighters[2].hp);
-	}
+	if (!enemySelected) {
+		enemySelected = true;
+		$("#enemy3").attr("src", "assets/images/placeholder.jpg");
+		
+		if (characterSelected && currentCharacterIndex === 3) {	
+			currentEnemyIndex = 2;
+			$("#currentEnemy").attr("src", "assets/images/characters/tidus.png");
+			$("#hpLevels").html("<br>" + fighters[2].name + "'s HP: " + fighters[2].hp);
+		}
 
-	else if (characterSelected) {
-		currentEnemyName = fighters[3].name;
-		currentEnemyIndex = 3;
-		$("#currentEnemy").attr("src", "assets/images/characters/tidus.png");
-		$("#hpLevels").html("<br>" + fighters[3].name + "'s HP: " + fighters[3].hp);
+		else if (characterSelected) {
+			currentEnemyIndex = 3;
+			$("#currentEnemy").attr("src", "assets/images/characters/tidus.png");
+			$("#hpLevels").html("<br>" + fighters[3].name + "'s HP: " + fighters[3].hp);
+		}
 	}
 })
-
-}
 
 
 
@@ -230,23 +226,26 @@ $("#attackButton").click (function() {
 		if (fighters[currentEnemyIndex].hp > 0) {
 			fighters[currentEnemyIndex].hp = fighters[currentEnemyIndex].hp - fighters[currentCharacterIndex].attack;
 			fighters[currentCharacterIndex].hp = fighters[currentCharacterIndex].hp - fighters[currentEnemyIndex].counterAttack;
-			$("#opponentAttack").html(fighters[currentEnemyIndex].name + " attacks you for " + fighters[currentEnemyIndex].counterAttack + " damage");
 			$("#yourAttack").html("You attack " + fighters[currentEnemyIndex].name + " for " + fighters[currentCharacterIndex].attack + " damage")
+			$("#opponentAttack").html(fighters[currentEnemyIndex].name + " attacks you for " + fighters[currentEnemyIndex].counterAttack + " damage");
 			$("#hpLevels").html(fighters[currentCharacterIndex].name + "'s HP: " + fighters[currentCharacterIndex].hp + "<br>");
 			if (fighters[currentEnemyIndex].hp > 0) {
 				$("#hpLevels").append(fighters[currentEnemyIndex].name + "'s HP: " + fighters[currentEnemyIndex].hp);
+				fighters[currentCharacterIndex].attack = fighters[currentCharacterIndex].attack * 2;
 			} else {
-				$("#hpLevels").append(fighters[currentEnemyIndex].name + "'s HP: 0");
+				fighters[currentEnemyIndex].hp = 0
+				$("#hpLevels").append(fighters[currentEnemyIndex].name + "'s HP: " + fighters[currentEnemyIndex].hp);
+				$("#currentEnemy").attr("src", "assets/images/chooseopponent.jpg");
+				$("#battleStats").html("You've defeated " + fighters[currentEnemyIndex].name + "<br>You have" + fighters[currentCharacterIndex].hp + " remaining.");
+				enemySelected = false;
 			}
-			fighters[currentCharacterIndex].attack = fighters[currentCharacterIndex].attack * 2;
-		} else {
-			fighters[currentEnemyIndex].hp = 0
-			$("#currentEnemy").attr("src", "assets/images/chooseopponent.jpg");
-			$("#battleStats").html("You've defeated " + fighters[currentEnemyIndex].name + "<br>You have" + fighters[currentCharacterIndex].hp + " remaining.");
+			
 		}
 })
 
-
+$("#restartButton").click (function() {
+	window.location.reload(true);
+})
 
 
 
